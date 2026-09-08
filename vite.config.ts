d293e25cca46485a6d35f5ad1812fe218850ccdd
Chiranth <chiranth01m@gmail.com>
2026-09-08 23:59:00 +0530

    feat: build MoneyLeap homepage

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7290',
+        secure: false,
+      },
+    },
+  },
 });
